e3cf76eb7575849d04937b27ab92f8845468f084
Fazal-e-Mabood <paksoft18@gmail.com>
2021-08-19 15:25:15 +0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -18,5 +18,25 @@ module.exports = {
   "5": {
     "name": "Ahmed",
     "birthday": "03-09"
+  },
+  "26": {
+    "name": "Fazal",
+    "birthday": "10-18"
+  },
+  "27": {
+    "name": "Arslan",
+    "birthday": "01-08"
+  },
+  "28": {
+    "name": "Faizan",
+    "birthday": "01-22"
+  },
+  "29": {
+    "name": "Ashfaq",
+    "birthday": "03-04"
+  },
+  "30": {
+    "name": "Ahmed",
+    "birthday": "03-09"
   }
 }
